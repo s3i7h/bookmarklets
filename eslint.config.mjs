b3a21879a62df4c70ef9d55120bc8c0a4687ad86
Yuichiro Luke Smith <contact@yu-smith.com>
2024-04-13 16:36:15 +0900

    feat: ✨ add @stylistic/eslint-plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,14 +3,21 @@ import globals from "globals";
 import path from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import pluginJs from "@eslint/js";
+import stylistic from '@stylistic/eslint-plugin';
 
 // mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
+const compat = new FlatCompat({ baseDirectory: __dirname });
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   ...compat.extends("airbnb"),
+  stylistic.configs['disable-legacy'],
+  stylistic.configs.customize({
+    indent: 2,
+    quotes: 'double',
+    semi: false,
+    "max-len": 120,
+  }),
 ];
